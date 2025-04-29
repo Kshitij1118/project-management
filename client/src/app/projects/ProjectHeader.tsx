53e0f7867c91301205086bf8d8fc15cb90293a0e
Kshitij Singh <kshitij1118@gmail.com>
2025-04-29 17:14:53 +0530

    few changes

--- client/src/app/projects/ProjectHeader.tsx
+++ client/src/app/projects/ProjectHeader.tsx
@@ -1,96 +1,114 @@
-import { useAppSelector } from "@/app/redux";
-import { useGetTasksQuery } from "@/state/api";
-import { DisplayOption, Gantt, ViewMode } from "gantt-task-react";
-import "gantt-task-react/dist/index.css";
-import React, { useMemo, useState } from "react";
+import Header from "@/components/Header";
+import {
+  Clock,
+  Filter,
+  Grid3x3,
+  List,
+  PlusSquare,
+  Share2,
+  Table,
+} from "lucide-react";
+import React, { useState } from "react";
+import ModalNewProject from "./ModalNewProject";
 
 type Props = {
-  id: string;
-  setIsModalNewTaskOpen: (isOpen: boolean) => void;
+  activeTab: string;
+  setActiveTab: (tabName: string) => void;
 };
 
-type TaskTypeItems = "task" | "milestone" | "project";
-
-const Timeline = ({ id, setIsModalNewTaskOpen }: Props) => {
-  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
-  const {
-    data: tasks,
-    error,
-    isLoading,
-  } = useGetTasksQuery({ projectId: Number(id) });
-
-  const [displayOptions, setDisplayOptions] = useState<DisplayOption>({
-    viewMode: ViewMode.Month,
-    locale: "en-US",
-  });
-
-  const ganttTasks = useMemo(() => {
-    return (
-      tasks?.map((task) => ({
-        start: new Date(task.startDate as string),
-        end: new Date(task.dueDate as string),
-        name: task.title,
-        id: `Task-${task.id}`,
-        type: "task" as TaskTypeItems,
-        progress: task.points ? (task.points / 10) * 100 : 0,
-        isDisabled: false,
-      })) || []
-    );
-  }, [tasks]);
-
-  const handleViewModeChange = (
-    event: React.ChangeEvent<HTMLSelectElement>,
-  ) => {
-    setDisplayOptions((prev) => ({
-      ...prev,
-      viewMode: event.target.value as ViewMode,
-    }));
-  };
-
-  if (isLoading) return <div>Loading...</div>;
-  if (error || !tasks) return <div>An error occurred while fetching tasks</div>;
+const ProjectHeader = ({ activeTab, setActiveTab }: Props) => {
+  const [isModalNewProjectOpen, setIsModalNewProjectOpen] = useState(false);
 
   return (
     <div className="px-4 xl:px-6">
-      <div className="flex flex-wrap items-center justify-between gap-2 py-5">
-        <h1 className="me-2 text-lg font-bold dark:text-white">
-          Project Tasks Timeline
-        </h1>
-        <div className="relative inline-block w-64">
-          <select
-            className="focus:shadow-outline block w-full appearance-none rounded border border-gray-400 bg-white px-4 py-2 pr-8 leading-tight shadow hover:border-gray-500 focus:outline-none dark:border-dark-secondary dark:bg-dark-secondary dark:text-white"
-            value={displayOptions.viewMode}
-            onChange={handleViewModeChange}
-          >
-            <option value={ViewMode.Day}>Day</option>
-            <option value={ViewMode.Week}>Week</option>
-            <option value={ViewMode.Month}>Month</option>
-          </select>
-        </div>
+      <ModalNewProject
+        isOpen={isModalNewProjectOpen}
+        onClose={() => setIsModalNewProjectOpen(false)}
+      />
+      <div className="pb-6 pt-6 lg:pb-4 lg:pt-8">
+        <Header
+          name="Product Design Development"
+          buttonComponent={
+            <button
+              className="flex items-center rounded-md bg-blue-primary px-3 py-2 text-white hover:bg-blue-600"
+              onClick={() => setIsModalNewProjectOpen(true)}
+            >
+              <PlusSquare className="mr-2 h-5 w-5" /> New Boards
+            </button>
+          }
+        />
       </div>
 
-      <div className="overflow-hidden rounded-md bg-white shadow dark:bg-dark-secondary dark:text-white">
-        <div className="timeline">
-          <Gantt
-            tasks={ganttTasks}
-            {...displayOptions}
-            columnWidth={displayOptions.viewMode === ViewMode.Month ? 150 : 100}
-            listCellWidth="100px"
-            barBackgroundColor={isDarkMode ? "#101214" : "#aeb8c2"}
-            barBackgroundSelectedColor={isDarkMode ? "#000" : "#9ba1a6"}
+      {/* TABS */}
+      <div className="flex flex-wrap-reverse gap-2 border-y border-gray-200 pb-[8px] pt-2 dark:border-stroke-dark md:items-center">
+        <div className="flex flex-1 items-center gap-2 md:gap-4">
+          <TabButton
+            name="Board"
+            icon={<Grid3x3 className="h-5 w-5" />}
+            setActiveTab={setActiveTab}
+            activeTab={activeTab}
+          />
+          <TabButton
+            name="List"
+            icon={<List className="h-5 w-5" />}
+            setActiveTab={setActiveTab}
+            activeTab={activeTab}
+          />
+          <TabButton
+            name="Timeline"
+            icon={<Clock className="h-5 w-5" />}
+            setActiveTab={setActiveTab}
+            activeTab={activeTab}
+          />
+          <TabButton
+            name="Table"
+            icon={<Table className="h-5 w-5" />}
+            setActiveTab={setActiveTab}
+            activeTab={activeTab}
           />
         </div>
-        <div className="px-4 pb-5 pt-1">
-          <button
-            className="flex items-center rounded bg-blue-primary px-3 py-2 text-white hover:bg-blue-600"
-            onClick={() => setIsModalNewTaskOpen(true)}
-          >
-            Add New Task
+        <div className="flex items-center gap-2">
+          <button className="text-gray-500 hover:text-gray-600 dark:text-neutral-500 dark:hover:text-gray-300">
+            <Filter className="h-5 w-5" />
           </button>
+          <button className="text-gray-500 hover:text-gray-600 dark:text-neutral-500 dark:hover:text-gray-300">
+            <Share2 className="h-5 w-5" />
+          </button>
+          <div className="relative">
+            <input
+              type="text"
+              placeholder="Search Task"
+              className="rounded-md border py-1 pl-10 pr-4 focus:outline-none dark:border-dark-secondary dark:bg-dark-secondary dark:text-white"
+            />
+            <Grid3x3 className="absolute left-3 top-2 h-4 w-4 text-gray-400 dark:text-neutral-500" />
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default Timeline;
+type TabButtonProps = {
+  name: string;
+  icon: React.ReactNode;
+  setActiveTab: (tabName: string) => void;
+  activeTab: string;
+};
+
+const TabButton = ({ name, icon, setActiveTab, activeTab }: TabButtonProps) => {
+  const isActive = activeTab === name;
+
+  return (
+    <button
+      className={`relative flex items-center gap-2 px-1 py-2 text-gray-500 after:absolute after:-bottom-[9px] after:left-0 after:h-[1px] after:w-full hover:text-blue-600 dark:text-neutral-500 dark:hover:text-white sm:px-2 lg:px-4 ${
+        isActive ? "text-blue-600 after:bg-blue-600 dark:text-white" : ""
+      }`}
+      onClick={() => setActiveTab(name)}
+    >
+      {icon}
+      {name}
+    </button>
+  );
+};
+
+export default ProjectHeader;
